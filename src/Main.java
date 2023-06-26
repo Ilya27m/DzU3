0fab696931f53b1e0984a7f9cca48b37794d34c5
Ilya27m <126259888+Ilya27m@users.noreply.github.com>
2023-06-26 11:30:58 +0300

    dfghmj

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        MyBinaryTree b = new MyBinaryTree();
+        b.add(4);
+        b.add(6);
+        b.add(7);
+        b.add(9);
+        b.add(8);
+        b.add(1);
+        b.add(4);
+        b.add(5);
+        b.add(3);
+        b.add(2);
+        b.add(0);
+        b.printTree();
+        b.remove(4);
+        b.remove(4);
+        b.remove(5);
+        b.remove(6);
+        b.remove(3);
+        b.printTree();
     }
 }
